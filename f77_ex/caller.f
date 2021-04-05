0b7c7458faa2566eb92e58ba3cc9c4721fb465b6
Mark Piper <mark.piper@colorado.edu>
2021-04-05 10:20:55 -0600

    Add simple f77 example

---
+++ f77_ex/caller.f
@@ -0,0 +1,12 @@
+      program callex
+      integer m, n
+c
+      m = 1
+      n = 2 
+
+      call iswap(m, n)
+      write(*,*) m, n
+
+      stop
+      end
+      
---
+++ f77_ex/iswap.f
@@ -0,0 +1,11 @@
+      subroutine iswap (a, b)
+      integer a, b
+c Local variables
+      integer tmp
+
+      tmp = a
+      a = b
+      b = tmp
+
+      return
+      end
