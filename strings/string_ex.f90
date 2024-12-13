7ccc23d39f96355de45897c9b57ea585134463cf
Mark Piper <mark.piper@colorado.edu>
2024-12-13 13:44:13 -0700

    Include more strings to evaluate

--- strings/string_ex.f90
+++ strings/string_ex.f90
@@ -4,12 +4,27 @@ program string_ex
     implicit none
     
     character(len=*), parameter :: filename = "/home/mpiper/foo.md"
+    character(len=1) :: delimiter
 
     write(*, "(a)") "Hello!"
 
     write(*, "(a)") "Hello from " // filename
 
-    ! Some questionable code, along with a refactor
+    ! Questionable code, along with a refactor
     write(*, '(/'' FILE '',a, '' DOES NOT EXIST'')') trim(filename)
     write(*, "(/' FILE ',a, ' DOES NOT EXIST')") trim(filename)
+
+    ! Questionable code
+    ! delimiter = "@"
+    delimiter = "\"
+    write(*, "(a)") "Delimiter outside conditional: " // delimiter
+    if (delimiter=='/'.or.delimiter=='\') then
+        write(*, "(a)") "Delimiter inside conditional: " // delimiter
+    endif
+
+    ! How does this work?
+    write(*, '(1x,a/1x,a/1x,a)') 'Could not obtain home directory path.', &
+        'Restart program and manually insert path', &
+        'to folder/directory containing input files.'
+
 end program string_ex
